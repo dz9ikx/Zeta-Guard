856b604092f6b91eb3c1772c346ed0cdd9f9eb07
dz9ikx <dz9ikx@gmail.com>
2026-01-20 20:28:13 +0200

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,11 @@
+"""
+Zeta-Guard: AI Training Stability Guardian
+"""
+
+from .guard import ZetaGuard, StabilityResult
+from . import protocols
+from . import utils
+
+__version__ = "0.0.1"
+__author__ = "dz9ikx"
+__all__ = ["ZetaGuard", "StabilityResult"]
