df4f943d14682589865144ec46650e1707f87f16
dz9ikx <dz9ikx@gmail.com>
2026-01-20 21:05:05 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,12 +2,7 @@
 Zeta-Guard: AI Training Stability Guardian
 """
 
-try:
-    
-    from .guard import ZetaGuard, StabilityResult
-except ImportError:
-    
-    from guard import ZetaGuard, StabilityResult
+from .guard import ZetaGuard, StabilityResult
 
 __version__ = "0.0.1"
 __author__ = "dz9ikx"
