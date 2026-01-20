355a267ad9b6629bd633b31c62fdf4eb53642f07
dz9ikx <dz9ikx@gmail.com>
2026-01-20 21:08:04 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="zeta-guard",
     version="0.0.1",
-    py_modules=["guard"],  # ⬅️ module
+    py_modules=["guard"],  
     install_requires=[
         "torch>=1.9.0",
         "numpy>=1.19.0",
